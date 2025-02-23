ddcdc3e05ecd85cb98e7495761bed8f679aef38b
isthathowitworks <ianwho721@gmail.com>
2025-02-23 19:49:35 +0800

    Add files via upload

---
+++ jascript.js
@@ -0,0 +1,4 @@
+let num1 = Number(prompt("Enter the first number you want to add:"));
+let num2 = Number(prompt("Enter the second number you want to add to the first number:"));
+let sum = num1 + num2;
+alert("The sum is: " + sum); 
