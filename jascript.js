67fd80a572fbf4dd459f236ade60d23bbddc33f8
isthathowitworks <ianwho721@gmail.com>
2025-02-23 19:49:08 +0800

    Delete jascript.js

--- jascript.js
+++
@@ -1,4 +0,0 @@
-let num1 = Number(prompt("Enter the first number you want to add:"));
-let num2 = Number(prompt("Enter the second number you want to add to the first number:"));
-let sum = num1 + num2;
-alert("The sum is: " + sum); 
